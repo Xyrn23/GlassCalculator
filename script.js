94f7507c45d4fa0569c11756640a08964867c081
xyrnzyrnx <93040266+Xyrn23@users.noreply.github.com>
2023-04-05 22:23:45 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// Add the functionality to your keyboard buttons
+const buttons = document.querySelectorAll(".button");
+
+buttons.forEach((button) => {
+  button.addEventListener("click", (event) => {
+    // Handle button click
+  });
+});
