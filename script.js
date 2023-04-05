9eca40eccf9355199293ac09d353ee1d9156bf79
xyrnzyrnx <93040266+Xyrn23@users.noreply.github.com>
2023-04-05 22:28:58 +0800

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-// Add the functionality to your keyboard buttons
-const buttons = document.querySelectorAll(".button");
-
-buttons.forEach((button) => {
-  button.addEventListener("click", (event) => {
-    // Handle button click
-  });
-});
